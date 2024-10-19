e65418bc76fd7b9b20ca7fafa37f1449016bb340
maxim <ygstoresup@gmail.com>
2024-10-19 22:52:54 +0300

    Fixed fourth practice

--- Lab/Lab4/src/Arithmetic.java
+++ Lab/Lab4/src/Arithmetic.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.sql.Timestamp;
+
 public class Arithmetic {
     public static double add(Object a, Object b) {
         a = checkVariable(a);
@@ -20,14 +23,32 @@ public class Arithmetic {
     public static double div(Object a, Object b) {
         a = checkVariable(a);
         b = checkVariable(b);
-        if ((double)b == 0.0) throw new CustomUnsupportedOperationException("Division by zero");
+        if ((double)b == 0.0) {
+            logMessage("Division by zero: " + a.toString() + " / " + b.toString());
+            throw new CustomUnsupportedOperationException("Division by zero");
+        }
         return (double)a / (double)b;
     }
 
     public static Object checkVariable(Object a) {
-        if (!(a instanceof Number)) throw new CustomUnsupportedOperationException("Provided object can't be used as a number");
+        if (!(a instanceof Number)) {
+            logMessage("Provided object <" + a.getClass().getName() + " | toStr: " + a.toString() + "> can't be used as a number");
+            throw new CustomUnsupportedOperationException("Provided object can't be used as a number");
+        }
         if (a instanceof Float) return ((Float)a).doubleValue();
         return a;
+    }
+
+    private static void logMessage(String message) {
+        try (FileWriter fw = new FileWriter("log.txt", true); BufferedWriter bw = new BufferedWriter(fw)) {
+            File logFile = new File("log.txt");
+            logFile.createNewFile();
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            bw.write("[" + timestamp + "] CustomUnsupportedOperationException | " + message);
+            bw.newLine();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
 }
