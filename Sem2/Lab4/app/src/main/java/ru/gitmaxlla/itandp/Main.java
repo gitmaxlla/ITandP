da78ea6e8b7a83af4856d5cb6e64f4670568a96b
gitmaxlla <user@email>
2025-04-01 19:55:16 +0300

    removed unused references

--- Sem2/Lab4/app/src/main/java/ru/gitmaxlla/itandp/Main.java
+++ Sem2/Lab4/app/src/main/java/ru/gitmaxlla/itandp/Main.java
@@ -1,8 +1,6 @@
 package ru.gitmaxlla.itandp;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.Arrays;
 
